f6c168690f700415c99225ca2983d7d58f20f4b6
jessicasouza1 <jessicasilvadvorak@gmail.com>
2020-08-06 21:16:43 -0300

    dos listo

--- dos.js
+++ dos.js
@@ -12,5 +12,50 @@ Considerar que las categorías pueden ser: almacen, lácteos, limpieza o bebidas
 
 function mostrar()
 {
-	alert("Hola Mundo");
+	let x="si";
+	let nom;
+	let cat;
+	let prec=0;
+	let max=0
+	let txtmax;
+	let txtmaxlac;
+	let cantBeb=0;
+
+	while(x == "si"){ 
+
+		nom = prompt("Ingrese el nombre de un artículo: ");
+		cat = prompt("Ingrese su categoria: ").toLowerCase();
+		prec = parseInt(prompt("Ingrese su precio: "));
+
+		//i = i+1;
+
+		if (!(cat == "almacen" || cat == "lacteos" || cat == "lácteos" || cat == "limpieza" || cat == "bebidas")){
+			alert("Debe ingresar una categoria valida: (almacen, lácteos, limpieza o bebidas)");
+		}
+		else {
+			if (prec >= 0 && prec <= 1000){
+				if (prec > max) {
+					max = prec;
+					txtmax = "El artículo con mayor precio es: "+nom;
+					if (cat == "lacteos" || cat == "lácteos"){
+						txtmaxlac = "El artículo con mayor precio de la categoría lácteos es: "+nom;
+					}
+				}
+				else {
+				alert("Debe ingresar un precio valido: (no puede ser menor a $0 ni mayor a $1000)");
+			}
+			if (cat == "bebidas"){
+				cantBeb = cantBeb + 1;
+			}
+			}
+		}
+
+		x = prompt("Quieres introducir otro artículo?").toLowerCase();
+
+	}
+
+	document.write(txtmax+"<br>");
+	document.write(txtmaxlac+"<br>");
+	document.write("La cantidad de artículos ingresados de categoría bebidas es: "+cantBeb);
+
 }
